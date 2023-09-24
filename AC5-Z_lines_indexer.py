45c157ee06ba1b1ec3a7ee1102299e7f8a786a9e
Andrei <46579508+Andreisgl@users.noreply.github.com>
2023-09-24 19:38:39 -0300

    tktinter experimentation

--- AC5-Z_lines_indexer.py
+++ AC5-Z_lines_indexer.py
@@ -1,4 +1,4 @@
-
+#region
 words = ['apple', 'banana', 'cherry', 'grape', 'kiwi', 'orange', 'pear', 'strawberry']
 
 # Each entry is a tuple. First item in the tuple is the criterion name,
@@ -8,5 +8,70 @@ INDEXING_CRITERIA = (('CHARACTER', []),
                      ('MISSION', []),
                      ('ACE_STYLE', ['MERCENARY', 'SOLDIER', 'KNIGHT'])
                      )
+
+#endregion
+
+
+
+import tkinter as tk
+from tkinter import *
+from ttkwidgets.autocomplete import *
+
+
+def setup_root_window():
+    global root
+    root = tk.Tk()
+    root.title('AC5-Z_RADIO_LINES_ORGANIZER')
+
+    window_width = 400
+    window_heigth = 300
+
+    root.geometry(f'{window_width}x{window_heigth}')
+
+    root.config(bg='#ffffff')
+
+    
+ 
+
+
+
+def lines_input_prompt():
+    global root
+    frame = Frame(root, bg='#f25252')
+    frame.pack(expand=True)
+
+    Label(
+        frame, 
+        bg='#f25252',
+        font = ('Times',21),
+        text='Field data'
+        ).pack()
+
+    entry = AutocompleteCombobox(
+        frame,
+        text = 'aaaaa',
+        width=30, 
+        font=('Times', 18),
+        completevalues=INDEXING_CRITERIA[3][1]
+        )
+    entry.pack()
+
+def main():
+    setup_root_window()
+    lines_input_prompt()
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
 
 
