8e7605aabbaf54d6dedffdcd80e1bfd0bac4dc91
Andrei <46579508+Andreisgl@users.noreply.github.com>
2023-09-24 17:49:11 -0300

    setup file and venv

---
+++ AC5-Z_lines_indexer.py
@@ -0,0 +1,10 @@
+## AC5-Z_lines_indexer by Truc3 (Andreisgl @ Github, @truc3_8492 @ Twitter)
+##===============================================================
+
+# This script runs through the extracted audio lines in a folder
+# and reproduces them one by one. Upon hearing it, you can enter
+# the information you know about it as prompted. This metadata 
+# will be saved to a .csv file for indexing.
+
+import os
+import PyQt5
